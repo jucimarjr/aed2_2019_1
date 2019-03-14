9ef52a408728c3ecf129c6da329f10ecb5d35ec5
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2019-03-14 19:26:02 -0400

    Update radix_sort.c

--- radix_sort.c
+++ radix_sort.c
@@ -6,28 +6,28 @@ void radix_sort(int numbers[], int size)
     int i;
     int *b;
     int max = numbers[0];
-    int count = 1;
+    int exp = 1;
 
     b = (int *)calloc(size, sizeof(int));
 
     for (i = 0; i < size; i++)
-	{
+    {
         if (numbers[i] > max)
-    	    max = numbers[i];
+            max = numbers[i];
     }
 
-    while (max/count > 0)
-	{
+    while (max/exp > 0)
+    {
         int bucket[10] = { 0 };
-    	for (i = 0; i < size; i++)
-    	    bucket[(numbers[i] / count) % 10]++;
-    	for (i = 1; i < 10; i++)
-    	    bucket[i] += bucket[i - 1];
-    	for (i = size - 1; i >= 0; i--)
-    	    b[--bucket[(numbers[i] / count) % 10]] = vetor[i];
-    	for (i = 0; i < size; i++)
-    	    numbers[i] = b[i];
-    	count *= 10;
+        for (i = 0; i < size; i++)
+            bucket[(numbers[i] / exp) % 10]++;
+        for (i = 1; i < 10; i++)
+            bucket[i] += bucket[i - 1];
+        for (i = size - 1; i >= 0; i--)
+            b[--bucket[(numbers[i] / exp) % 10]] = numbers[i];
+        for (i = 0; i < size; i++)
+            numbers[i] = b[i];
+        exp *= 10;
     }
 
     free(b);
