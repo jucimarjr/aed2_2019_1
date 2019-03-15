7677c5c2df127a2a181459283a8841d9ccbe3b95
Botinelly <lucas_botinelly@hotmail.com>
2019-03-14 23:00:09 -0400

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,27 +1,33 @@
 #include "io.h"
+#define TAM 1000
 
-void bubble_sort(int numbers[])
+int bubble_sort(int vector[])
 {
 	int aux;
+	int vector_aux[TAM] = vector[];
 	for(int counter = 1 ; counter < TAM ; counter++)
 	{
 		for(int i = 0 ; i < TAM -1 ; i++)
 		{
-			if(numbers[i] > numbers[i+1])
+			if(vector_aux[i] > vector_aux[i+1])
 			{
-				aux = numbers[i];
-				numbers[i] = numbers[i+1];
-				numbers[i+1] = aux;
+				aux = vector_aux[i];
+				vector_aux[i] = vector_aux[i+1];
+				vector_aux[i+1] = aux;
 			}
 		}
 	}
+    return vector_aux;
 }
 
 int main()
 {
-	int vector[TAM];
-	read(vector);
-	bubble_sort(vector);
-	print(vector);
+   	int vector[TAM];
+   	int result[TAM];
+
+   	vector = read(vector);
+	result = bubble_sort(vector);
+	print(result);
+
 	return 0;
 }
