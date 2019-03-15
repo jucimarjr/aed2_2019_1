e7f5d7e22e511080d54095cf7874416b0dffde31
aamandachaves <a.amandachaves@outlook.com>
2019-03-15 00:48:35 -0400

    shell_sort update

--- shell_sort.c
+++ shell_sort.c
@@ -1,43 +1,40 @@
 #include "io.h"
 
-void shell_sort(int *a, int size);
-
-void shell_sort(int *vector, int size)
+void shell_sort(int vector[])
 {
- int i, j, value;
- int gap = 1;
+    int i, j, value;
+    int gap = 1;
 
- do
- {
-   gap = 3 * gap + 1;
- }while(gap < size);
+    do
+    {
+        gap = 3 * gap + 1;
+    } while(gap < TAM);
 
- do
- {
-   gap /= 3;
-   for(i = gap; i < size; i++)
-   {
-     value = vector[i];
-     j = i - gap;
+    do
+    {
+        gap /= 3;
+        for(i = gap; i < TAM; i++)
+        {
+            value = vector[i];
+            j = i - gap;
 
-     while (j >= 0 && value < vector[j])
-     {
-       vector[j + gap] = vector[j];
-       j -= gap;
-     }
+            while (j >= 0 && value < vector[j])
+            {
+                vector[j + gap] = vector[j];
+                j -= gap;
+            }
 
-     vector[j + gap] = value;
-  }
- }while(gap > 1);
+            vector[j + gap] = value;
+        }
+    } while(gap > 1);
 }
-/*
+
 int main()
 {
-  int vector[TAM];
-  read(vector, TAM);
-  shell_sort(vector, TAM);
-  print(vector,TAM);
+    int vector[TAM];
+    read(vector);
+    shell_sort(vector);
+    print(vector);
 
-  return 0;
+    return 0;
 }
-*/
